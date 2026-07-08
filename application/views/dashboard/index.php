a64058a03c1051168c22d04f5fb60799d9f031e4
Dzaky Renaldy <dzakyreviewfilm@gmail.com>
2026-07-08 13:33:38 +0700

    Perbaikan warna pada dashboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -32,7 +32,7 @@
             background-color: #ffffff;
             padding: 12px 0;
             transition: all 0.3s ease-in-out;
-            border-bottom: 2px solid #ea5b1a;
+            border-bottom: 5px solid #ea5b1a;
             box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
         }
         .navbar-dark .navbar-nav .nav-link {
@@ -111,7 +111,7 @@
 
         /* SOP / Aturan Section */
         .sop-section {
-            background-color: #5d3315; 
+            background-color: #343434; 
             background-image: url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.03"%3E%3Cpath d="M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E');
             padding: 80px 0;
             color: white;
